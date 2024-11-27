43309b7f66c3d647b8ca72dfc48dba31d51732a2
gmtev <145558375+gmtev@users.noreply.github.com>
2024-11-27 20:10:56 +0200

    remove unnecessary data

--- CarsExamPrep/CarsExamPrep/settings.py
+++ CarsExamPrep/CarsExamPrep/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w#rx2&gjt$yz6!nlih_pd2l9!5*38mh$*fu-j_8=3y)ywau9#f'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
